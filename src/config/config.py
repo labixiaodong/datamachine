609b7cae973c6ce21c3bc36121f74ca00ea40fe6
debulaonei <15026578506@163.com>
2020-06-15 23:27:57 +0800

    dss

---
+++ src/config/config.py
@@ -0,0 +1,11 @@
+
+"""
+数据库信息
+"""
+
+big_data = {}
+big_data['host'] = '47.95.140.2'
+big_data['port'] = '3306'
+big_data['user'] = 'root'
+big_data['passwd'] = 'jxd200599'
+big_data['db'] = 'wangzhe'
