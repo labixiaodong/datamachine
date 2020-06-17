8614aeeb9909beecfa43d9510712f80958653f27
debulaonei <1546442316@qq.com>
2020-06-17 17:19:11 +0800

    add mysqlhlper

--- src/utils/mysql_help.py
+++ src/utils/mysql_help.py
@@ -1,6 +1,32 @@
+from utils.db_pools import getDBonnectionPools
+import pandas as pd 
+from config import config
+import pymysql
+import os
+import time
 
 
+DB = config.big_data
+host = DB["host"]
+port = DB["port"]
+user = DB["user"]
+passwd = DB["passwd"]
+db = DB["db"]
 
-"""
-用于连接mysql，提供增删改查接口
-"""
+class MysqlHelper(object):
+
+    def __init__(self,
+                 host=host, port=port, user=user, passwd=passwd, db=db, charset='utf8', read_timeout=5400, pools=False):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+        self.db = db
+        self.charset = charset
+        self.read_timeout = read_timeout
+        self.pools = pools
+        self.conn = None
+        self.cursor = None
+
+        if self.pools:
+            self.dbinstance = getDBonnectionPools()
