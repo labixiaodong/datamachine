922ff45c22cea467f34fa43146c4c2c62de86cbf
debulaonei <1546442316@qq.com>
2020-06-18 15:19:10 +0800

    sda

--- src/utils/mysql_help.py
+++ src/utils/mysql_help.py
@@ -30,3 +30,76 @@ class MysqlHelper(object):
 
         if self.pools:
             self.dbinstance = getDBonnectionPools()
+
+    def insertdata_bydf(self, df, tb, if_exists='append', n=5):
+
+        start_time = time.time()   # 返回当前时间戳
+
+        # 对传入的df进行清洗，替换nan为mysql接受的None，并且转为字符串
+        df = df.where(pd.notnull(df), "None").replace("nan", "None").replace("NaN", "None")
+        df = df.astype("str")
+
+        # 组合sql语句
+        sql = """insert into {0} ({1}) values ({2});"""
+        sql = sql.format(tb, ",".join(df.columns), ("%s," * len(df.columns))[:-1])
+
+        count_times = 0
+        while count_times < n:
+            try:
+                conn = self.getconn()
+                cursor = conn.cursor()
+
+                if if_exists == 'replace':
+                    delete_rows = cursor.execute('delete from {0}'.format(tb))
+                elif if_exists == 'replace-truncate':
+                    delete_rows = cursor.execute('truncate from {0}'.format(tb))
+                else:
+                    delete_rows = 0
+
+                para = [tuple([None if y == "None" else y for y in x]) for x in df.values]
+                insert_rows = cursor.executemany(sql, para)
+                conn.commit()
+
+                print("   insert数据行数:{0}".format(insert_rows))
+                print("数据库insert成功")
+
+                endTime = time.time()
+                time_eclipse = round((endTime - startTime), 2)
+
+                count_times = n
+
+            except Exception as e:
+
+                count_times += 1
+                conn.rollback()
+                conn.commit()
+                self.close(conn, cursor)
+
+                time.sleep(10)
+
+                if count_times >= n:
+                    print('insertmany_bydf error execute:')
+                    raise Exception("数据插入失败")
+
+
+
+
+
+    def getconn(self):
+        if self.pools:
+            conn = self.dbinstance.getconn(self.host, self.port, self.user, self.passwd, self.db, self.charset)
+        else:
+            conn = pymysql.connect(
+                host=self.host,
+                port=self.port,
+                user=self.user,
+                passwd=self.passwd,
+                db=self.db,
+                read_timeout=self.read_timeout,
+                charset=self.charset,
+            )
+            print("\n数据库连接成功")
+        return conn
+
+
+
